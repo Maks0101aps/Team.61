6c5c750be665c90865f46475d5b61d6e81e80472
Maksym Lysak <maksymlysak@gmail.com>
2025-04-09 15:05:05 +0300

    Fixed calendar view switching issue that caused the calendar to disappear

--- resources/js/Shared/Calendar.vue
+++ resources/js/Shared/Calendar.vue
@@ -181,6 +181,17 @@
         </div>
       </div>
     </div>
+    
+    <!-- Fallback View (during refresh) -->
+    <div v-else class="bg-white rounded-xl shadow-lg overflow-hidden min-h-[400px] flex items-center justify-center">
+      <div class="text-blue-500">
+        <svg class="animate-spin h-10 w-10 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+          <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+        <p class="mt-3 text-center text-sm">{{ language === 'uk' ? 'Оновлення календаря...' : 'Updating calendar...' }}</p>
+      </div>
+    </div>
 
     <!-- Event Details Modal -->
     <div v-if="selectedEvent" 
@@ -282,10 +293,10 @@ export default {
       if (currentView.value === 'year') {
         // For year view, we need to update all month names
         // This is a hacky way to force Vue to re-render
-        const temp = currentView.value
-        currentView.value = 'temp'
+        const currentViewValue = currentView.value
+        currentView.value = '_refresh'
         setTimeout(() => {
-          currentView.value = temp
+          currentView.value = currentViewValue
         }, 10)
       }
     }
@@ -295,10 +306,11 @@ export default {
       updateLocale()
       
       // Force a refresh of the view to update all translated elements
-      const temp = currentView.value
-      currentView.value = 'temp'
+      // Don't use 'temp' as a value since it's not a valid view
+      const currentViewValue = currentView.value
+      currentView.value = '_refresh'
       setTimeout(() => {
-        currentView.value = temp
+        currentView.value = currentViewValue
       }, 10)
     }, { immediate: true })
 
@@ -540,10 +552,13 @@ export default {
         case 'year':
           currentDate.value = currentDate.value.subtract(1, 'year')
           break
+        default:
+          // Don't attempt to modify if we're in a temporary state
+          return
       }
       
       // Force refresh of locale-sensitive computed properties
-      updateLocale()
+      // No need to modify the view here as we're just changing the date
     }
 
     const nextPeriod = () => {
@@ -563,10 +578,13 @@ export default {
         case 'year':
           currentDate.value = currentDate.value.add(1, 'year')
           break
+        default:
+          // Don't attempt to modify if we're in a temporary state
+          return
       }
       
       // Force refresh of locale-sensitive computed properties
-      updateLocale()
+      // No need to modify the view here as we're just changing the date
     }
 
     const createEventOnDate = (date) => {
